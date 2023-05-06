52eeff2370d651af49e797034e018459c97fd51b
Beverly <bc2542@nyu.edu>
2023-05-05 19:52:34 -0700

    update letter spacing

--- src/workData.js
+++ src/workData.js
@@ -5,8 +5,14 @@ export const workData = {
     year: 2018,
     images: [
       { url: "https://placekitten.com/200/300", caption: "wow" },
-      { url: "https://placekitten.com/600/300", caption: "here's a longer one lolol " },
-      { url: "https://placekitten.com/400/800", caption: "OOOOOOOO WOWOWOWOWOWOWOWOWOWOWOWO" },
+      {
+        url: "https://placekitten.com/600/300",
+        caption: "here's a longer one lolol ",
+      },
+      {
+        url: "https://placekitten.com/400/800",
+        caption: "OOOOOOOO WOWOWOWOWOWOWOWOWOWOWOWO",
+      },
       { url: "https://placekitten.com/400/300", caption: "" },
     ],
     text: (
@@ -33,6 +39,37 @@ export const workData = {
     name: "Visual Mode",
     tags: ["interactive web", "node.js"],
     year: 2018,
+    images: [
+      { url: "https://placekitten.com/200/300", caption: "wow" },
+      {
+        url: "https://placekitten.com/600/300",
+        caption: "here's a longer one lolol ",
+      },
+      {
+        url: "https://placekitten.com/400/800",
+        caption: "OOOOOOOO WOWOWOWOWOWOWOWOWOWOWOWO",
+      },
+      { url: "https://placekitten.com/400/300", caption: "" },
+    ],
+    text: (
+      <span>
+        I prototyped a series of functional physical interfaces that communicate
+        with other devices. I built these projects using Arduino
+        microcontrollers (MKR1000, Micro, Uno, ESP8266), a variety of analog and
+        digital sensors, and different data protocols (sACN/DMX, HID/USB, MIDI,
+        TCP). Click on the images to learn more about each controller.I
+        prototyped a series of functional physical interfaces that communicate
+        with other devices. I built these projects using Arduino
+        microcontrollers (MKR1000, Micro, Uno, ESP8266), a variety of analog and
+        digital sensors, and different data protocols (sACN/DMX, HID/USB, MIDI,
+        TCP). Click on the images to learn more about each controller.I
+        prototyped a series of functional physical interfaces that communicate
+        with other devices. I built these projects using Arduino
+        microcontrollers (MKR1000, Micro, Uno, ESP8266), a variety of analog and
+        digital sensors, and different data protocols (sACN/DMX, HID/USB, MIDI,
+        TCP). Click on the images to learn more about each controller.
+      </span>
+    ),
   },
   aros: {
     name: "Aros for Visually Impaired Cooks",
