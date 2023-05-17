dbbf25477ef440fd89a2f86a68857b3675c8e86e
Beverly <bc2542@nyu.edu>
2023-05-17 00:15:03 -0700

    move fonts

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? "/portfolio-2023/" : "",
+  // assetPrefix: isProd ? "/portfolio-2023/" : "",
   images: {
     loader: "akamai",
     path: "",
