b3f90cee75c569bece547e2941526b07fd9e4946
Beverly <bc2542@nyu.edu>
2023-08-07 18:10:30 -0700

    add new redirect page

---
+++ src/pages/projects.js
@@ -0,0 +1,22 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+useRouter;
+
+const Projects = () => {
+  const router = useRouter();
+
+  // redirect to page
+  useEffect(() => {
+    router.push(
+      "https://www.figma.com/proto/Dq4yhm5HeqeUb0hOItR4xd/Bev---Focus-Portfolio-Share?page-id=113%3A16&type=design&node-id=113-17&viewport=281%2C89%2C0.19&t=8wjxzL1tR5BRcfe4-1&scaling=contain&mode=design"
+    );
+  }, []);
+
+  return (
+    <div className="notFound">
+      <div>redirecting...</div>
+    </div>
+  );
+};
+
+export default Projects;
